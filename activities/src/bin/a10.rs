4ae7fb93ac025e9c9259286c4da2841195c96218
caspero-62 <nwaforuchechukwu2007@gmail.com>
2024-10-31 20:30:51 +0100

    feat: activity 10 solved

--- activities/src/bin/a10.rs
+++ activities/src/bin/a10.rs
@@ -12,5 +12,18 @@
 // * Use a match expression to determine which message
 //   to print
 
-fn main() {}
+fn big_or_small(is_big: bool) {
+    match is_big {
+        true => println!("it's big"),
+        false => println!("it's small")
+    } 
+}
+
+fn main() {
+    let n = 120;
+
+    let is_big = n > 100;
+
+    big_or_small(is_big);   
+}
 
