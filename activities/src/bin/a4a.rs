ecf66bec605876c9f080b0ad3b08bfca30475f0f
caspero-62 <nwaforuchechukwu2007@gmail.com>
2024-10-31 16:09:28 +0100

    feat: activity 4a solved

--- activities/src/bin/a4a.rs
+++ activities/src/bin/a4a.rs
@@ -4,8 +4,15 @@
 // * Display "it's true" or "it's false" based on the value of a variable
 //
 // Notes:
-// * Use a variable set to either true or false
-// * Use a match expression to determine which message to display
 
-fn main() {}
+fn main() {
+    // * Use a variable set to either true or false
+    let is_of_age = true;
+    
+    // * Use a match expression to determine which message to display
+    match is_of_age {
+        true => println!("You are of age, enter club!"),
+        false => println!("You're not of age, get out!")
+    }
+}
 
