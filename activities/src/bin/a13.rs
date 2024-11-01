60892c231749dbaa6e8fc9ffaf9ef42443425762
caspero-62 <nwaforuchechukwu2007@gmail.com>
2024-11-01 17:08:03 +0100

    feat: activity 13 solved

--- activities/src/bin/a13.rs
+++ activities/src/bin/a13.rs
@@ -3,23 +3,23 @@
 // Requirements:
 // * Print 10, 20, "thirty", and 40 in a loop
 // * Print the total number of elements in a vector
-//
+
 // Notes:
-// * Use a vector to store 4 numbers
-// * Iterate through the vector using a for..in loop
-// * Determine whether to print the number or print "thirty" inside the loop
-// * Use the .len() function to print the number of elements in a vector
 
 fn main() {
+    // * Use a vector to store 4 numbers
     let my_vector = vec![10, 20, 30, 40];
-
+    
+    // * Iterate through the vector using a for..in loop
     for number in &my_vector {
+        // * Determine whether to print the number or print "thirty" inside the loop
         match number {
             30 => println!("thirty"),
             _ => println!("{:?}", number),
         };
     };
-
+    
+    // * Use the .len() function to print the number of elements in a vector
     println!("Length: {:?}", &my_vector.len());
 }
 
