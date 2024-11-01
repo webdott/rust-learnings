ca8b7bdeef21c79e1a0b0548c2255da2945c8019
caspero-62 <nwaforuchechukwu2007@gmail.com>
2024-11-02 00:38:24 +0100

    feat: activity 14 solved

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -2,14 +2,48 @@
 //
 // Requirements:
 // * Print out the name and favorite colors of people aged 10 and under
-//
+
 // Notes:
+
 // * Use a struct for a persons age, name, and favorite color
-// * The color and name should be stored as a String
-// * Create and store at least 3 people in a vector
-// * Iterate through the vector using a for..in loop
-// * Use an if expression to determine which person's info should be printed
-// * The name and colors should be printed using a function
+struct Person {
+    // * The color and name should be stored as a String
+    name: String,
+    fav_color: String,
+    age: i32
+}
 
-fn main() {}
+fn print_name_colors(person: &Person) {
+    // * The name and colors should be printed using a function
+    println!("Name: {:?}; Favorite color: {:?}", person.name, person.fav_color)
+}
 
+fn main() {
+    // * Create and store at least 3 people in a vector
+    let people = vec![
+        Person {
+            name: String::from("Uchechukwu"),
+            fav_color: String::from("Purple"),
+            age: 24
+        },
+        Person {
+            name: String::from("Azeez"),
+            fav_color: String::from("Blue"),
+            age: 23
+        },
+        Person {
+            name: String::from("Nonse"),
+            fav_color: String::from("Black"),
+            age: 24
+        },
+    ];
+        
+    // * Iterate through the vector using a for..in loop
+    for person in people {
+        // * Use an if expression to determine which person's info should be printed
+        if person.age >= 24 {
+            print_name_colors(&person);
+        }
+    }
+}
+    
