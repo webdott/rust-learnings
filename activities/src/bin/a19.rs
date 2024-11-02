67fbbff7508d12479377b89f98cd2aeff32c2e38
caspero-62 <nwaforuchechukwu2007@gmail.com>
2024-11-03 00:26:07 +0100

    feat: activity 19 solved

--- activities/src/bin/a19.rs
+++ activities/src/bin/a19.rs
@@ -13,5 +13,25 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+
+fn main() {
+    let mut hashmap = HashMap::new();
+
+    hashmap.insert("Chairs", 5);
+    hashmap.insert("Beds", 3);
+    hashmap.insert("Tables", 2);
+    hashmap.insert("Couches", 0);
+
+    let mut total_items_count = 0;
+
+    for (name, number) in hashmap.iter() {
+        println!("Name: {name:?}, ItemCount: {number:?}");
+
+        total_items_count = total_items_count + number
+    }
+
+    println!("Total Number of Items: {total_items_count:?}")
+}
 
