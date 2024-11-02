4eec94e16bc2f155e1b9e5fae5247c89e8ec1b68
caspero-62 <nwaforuchechukwu2007@gmail.com>
2024-11-02 01:20:48 +0100

    feat: activity 15 solved

--- activities/src/bin/a15.rs
+++ activities/src/bin/a15.rs
@@ -11,5 +11,25 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+enum Ticket {
+    Backstage(f64, String),
+    Standard(f64),
+    Vip(f64, String),
+}
+
+fn main() {
+    let tickets = vec![
+        Ticket::Backstage(70.0, String::from("Uchechukwu")),
+        Ticket::Standard(20.0),
+        Ticket::Vip(80.0, String::from("Azeez")),
+    ];
+
+    for ticket in tickets {
+        match  ticket {
+            Ticket::Backstage(price, holder) => println!("Holder: {:?}, Price: {:?}", holder, price),
+            Ticket::Standard(price) => println!("Price: {:?}", price),
+            Ticket::Vip(price, holder) => println!("Holder: {:?}, Price: {:?}", holder, price),
+        }
+    }
+}
 
